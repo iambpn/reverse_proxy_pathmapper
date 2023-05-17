1a68c555ee4604e5c337016c2bb15d3d23a83949
Bipin Maharjan <bopon.maharjan@gmail.com>
2023-05-17 22:00:45 +0545

    adding web socket support.

--- src/index.ts
+++ src/index.ts
@@ -12,12 +12,12 @@ export class ReverseProxyPathMapper {
   private path_regex: PathRegex[] = [];
 
   constructor(proxyOptions: httpProxy.ServerOptions, pathMapper: PathMapperType) {
-    this.proxy = httpProxy.createProxy<Request, Response>(proxyOptions);
-    this.initProxyErrorHandler();
+    this.proxy = httpProxy.createProxyServer<Request, Response>(proxyOptions);
+    this.initProxyEvents();
     this.initPathRegex(pathMapper);
   }
 
-  private initProxyErrorHandler() {
+  private initProxyEvents() {
     this.proxy.on("error", function (error, req, res) {
       console.log("Proxy Error:", error);
       return res.end("Error while connecting to proxy \n\n" + error.toString());
@@ -48,23 +48,43 @@ export class ReverseProxyPathMapper {
     }
   }
 
+  private getTargetUrl(req: Request): { target: string; error?: string } {
+    const target = this.matchURL(req);
+
+    if (target) {
+      return { target };
+    }
+
+    const errorMsg = `Path: ${req.url}  Did not matched.`;
+    return { target: "", error: errorMsg };
+  }
+
   serve(port: number) {
-    http
-      .createServer((req, res) => {
-        const target = this.matchURL(req);
+    const server = http.createServer((req, res) => {
+      const { target, error } = this.getTargetUrl(req);
 
-        if (target) {
-          // proxy request
-          return this.proxy.web(req, res, { target: target });
-        }
-
+      if (error) {
+        console.log(error);
         // Send res.end() with some message
-        const errorMsg = `Path: ${req.url}  Did not matched.`;
-        console.log(errorMsg);
-        return res.end(errorMsg);
-      })
-      .listen(port, () => {
-        console.log("Reverse Proxy listening on port " + port);
-      });
+        return res.end(error);
+      }
+
+      // forward proxy request
+      return this.proxy.web(req, res, { target: target });
+    });
+
+    server.on("upgrade", (req, socket, head) => {
+      const { target, error } = this.getTargetUrl(req);
+
+      if (error) {
+        return console.log(error);
+      }
+      // proxy ws request
+      return this.proxy.ws(req, socket, head, { target: target });
+    });
+
+    server.listen(port, () => {
+      console.log("Reverse Proxy listening on port " + port);
+    });
   }
 }
